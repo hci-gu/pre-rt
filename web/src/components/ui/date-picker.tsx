e5652161a62207306f6cfad07171993c7a967e2d
Sebastian Andreasson <sebastian.andreasson93@gmail.com>
2026-06-08 10:31:00 +0200

    datepicker fix

--- web/src/components/ui/date-picker.tsx
+++ web/src/components/ui/date-picker.tsx
@@ -6,42 +6,24 @@ import { sv } from 'date-fns/locale'
 
 import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
-import { Calendar } from '@/components/ui/calendar'
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/popover'
 
 export function DatePicker({
   date,
-  onChange,
+  onChange: _onChange,
 }: {
   date: Date | undefined
   onChange: (value: Date | undefined) => void
 }) {
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant={'outline'}
-          className={cn(
-            'w-[240px] justify-start text-left font-normal',
-            !date && 'text-muted-foreground'
-          )}
-        >
-          <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? format(date, 'PPP', { locale: sv }) : <span>Välj datum</span>}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-auto p-0" align="start">
-        <Calendar
-          mode="single"
-          selected={date}
-          onSelect={(date) => onChange(date)}
-          initialFocus
-        />
-      </PopoverContent>
-    </Popover>
+    <Button
+      variant={'outline'}
+      className={cn(
+        'w-[240px] justify-start text-left font-normal',
+        !date && 'text-muted-foreground'
+      )}
+    >
+      <CalendarIcon className="mr-2 h-4 w-4" />
+      {date ? format(date, 'PPP', { locale: sv }) : <span>Välj datum</span>}
+    </Button>
   )
 }
--- web/src/pages/welcome/index.tsx
+++ web/src/pages/welcome/index.tsx
@@ -9,15 +9,6 @@ function WelcomePage() {
         <h1 className="text-2xl font-semibold tracking-tight">
           Välkommen till studien
         </h1>
-        <p className="text-sm text-muted-foreground">
-          Pre-RT är ett forskningsprojekt som syftar till att förbättra
-          rådgivning och uppföljning av sexuell hälsa hos kvinnor efter
-          strålbehandling mot bäckenområdet. Tidigare forskning har
-          uppmärksammat att tidiga rehabiliteringsinsatser - prehabilitering -
-          kan vara fördelaktigt inom andra områden och vi vill undersöka om det
-          kan gälla även för att förebygga och rehabilitera vaginala
-          förändringar som kan uppstå i samband med strålbehandling.
-        </p>
       </div>
       <p className="px-8 text-center text-sm text-muted-foreground">
         För att se dina svar behöver du logga in.
